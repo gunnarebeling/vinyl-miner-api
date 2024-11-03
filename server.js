2d15fa2bf25f28d0eace933ac23ae5b2e60e386e
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-11-03 10:51:33 -0600

    update database with routes.json

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('database.json');
 const middlewares = jsonServer.defaults();
+const rewriter = jsonServer.rewriter('routes.json'); 
 
 server.use(middlewares);
+server.use(rewriter);
 server.use(router);
 
 const port = process.env.PORT || 3000;
